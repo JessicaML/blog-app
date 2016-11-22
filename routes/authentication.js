3a5cada626961bd6f7c6fdf7a756304932f7224a
Jessica Leach <jessicamaryleach@gmail.com>
2016-11-22 16:40:20 +0100

    validations on register users in progress

---
+++ routes/authentication.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    bcrypt = require('bcrypt'),
+    db = require('../models'),
+    router = express.Router();
+
+router.get('/register', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/admin/posts');
+  }
+
+  res.render('users/new');
+});
+
+router.post('/register', (req, res) => {
+  db.User.create(req.body).then((user) => {
+    req.session.user = user;
+    res.redirect('/');
+  }).catch((error) => {
+    res.render('users/new', { errors: error.errors });
+  });
+});
+
+router.post('/login', (req, res) => {
+  db.User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then((userInDB) => {
+    bcrypt.compare(req.body.password, userInDB.passwordDigest, (error, result) => {
+      if (result) {
+        req.session.user = userInDB;
+        res.direct('/');
+      } else {
+        res.render('login', { error: {message: 'Password is incorrect' } });
+      }
+    });
+  }).catch((error) => {
+    res.render('login', { error: { message: 'User not found in the database' } });
+  });
+});
+
+router.get('/logout', (req, res) => {
+  req.session.user = undefined;
+  res.redirect('/');
+});
+
+module.exports = router;
